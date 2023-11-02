b4a610de4e7fb0b4aa04e23653c26ab4e8e8d451
Priyanshu Jaiswal <priyanshuskt7839@gmail.com>
2023-11-02 19:40:55 +0530

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,12 @@
+// const {innerHeight} = window;
+
+// var tl=gsap.timeline();
+// tl.to(".gazing_eyes",{
+//     scale:10,stagger:0.25,duration:3,
+//     scrollTrigger:{
+//         trigger:"#flower_skull",
+//         pin:true,
+//         end:`+=${innerHeight * 1.3}`,
+//         scrub:3
+//     }
+// })
